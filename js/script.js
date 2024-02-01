e9cbd3e72c1dacef725280dc12594d6dd191e137
lecksa <dolabesipa@gmail.com>
2024-02-01 14:45:57 +0500

    added

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 //4
 import { movies } from "./db.js";
 import { numbers } from "./ui.js";
+import {reload_genres} from "./ui.js"
 
 let promo__interactive_ul = document.querySelector('.promo__interactive-list')
+let ul_menu = document.querySelector('.promo__menu-list ul')
+let search_inp = document.querySelector('#search')
 
 let genres = ['Все', ...new Set(movies.map(item => item.Genre))]
-console.log(genres);
 
-numbers(movies, promo__interactive_ul, genres)
+search_inp.onkeyup = (e) => {
+    let key_word = e.target.value.trim().toLowerCase()
+
+    let finded = movies.filter(item => {
+        let title = item.Title.toLocaleLowerCase()
+
+        if(title.includes(key_word)){
+            return item
+        }
+    })
+    numbers(finded, promo__interactive_ul)
+}
+
+reload_genres(genres, ul_menu, promo__interactive_ul)
+
+numbers(movies, promo__interactive_ul)
--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,22 @@
-export function numbers(arr, place, janri) {
+import { movies } from "./db.js"
+
+// function setMovie(item) {
+//     let genre = document.querySelector('.promo__genre')
+//     const rating = document.querySelector('.IMDb')
+//     const rating_2 = document.querySelector('.kino')
+//     const plot = document.querySelector('.promo__descr')
+//     const title = document.querySelector('.promo__title')
+//     const bg = document.querySelector('.promo__bg')
+
+//     bg.style.background = `url(${item.Poster}) center center/cover no-repeat`
+//     rating.innerHTML = 'IMDb:' + ' ' + `${item.imdbRating}`
+//     rating_2.innerHTML = 'Кинопоиск:' + ' ' + `${item.Kinopoisk}`
+//     plot.innerHTML = `${item.Plot}`
+//     genre.innerHTML = `${item.Genre}`
+//     title.innerHTML = `${item.Title}`
+// }
+
+export function numbers(arr, place) {
     //1
     let imgs = document.querySelectorAll('.promo__adv img')
     imgs.forEach(img => { img.remove() })
@@ -11,12 +29,12 @@ export function numbers(arr, place, janri) {
     let bg = document.querySelector('.promo__bg')
     bg.style.backgroundImage = `url(./img/bg.jpg)`
 
-
     place.innerHTML = ''
+
+    setMovie(arr[0])
 
     for (let item of arr) {
         let idx = arr.indexOf(item) + 1
-        console.log(idx, item);
 
         //a
         let promo__interactive_li = document.createElement('li')
@@ -33,58 +51,49 @@ export function numbers(arr, place, janri) {
 
         div_delete.onclick = () => {
             arr.splice(idx - 1, 1)
-            numbers(arr, place, janri)
+            numbers(arr, place)
         }
-
-        const rating = document.querySelector('.IMDb')
-        const rating_2 = document.querySelector('.kino')
-        const plot = document.querySelector('.promo__descr')
-        const title = document.querySelector('.promo__title')
-        const bg = document.querySelector('.promo__bg')
 
         promo__interactive_li.onclick = () => {
-
-            bg.style.background = `url(${item.Poster}) center center/cover no-repeat`
-            rating.innerHTML = 'IMDb:' + ' ' + `${item.imdbRating}`
-            rating_2.innerHTML = 'Кинопоиск:' + ' ' + `${item.Kinopoisk}`
-            plot.innerHTML = `${item.Plot}`
-            genre.innerHTML = `${item.Genre}`
-            title.innerHTML = `${item.Title}`
-        }
-
-        // movie(janri)
-        
-        let ul_menu = document.querySelector('.promo__menu-list ul')
-
-        // function movie(janrimov) {
-
-            ul_menu.innerHTML = ''
-
-            for (let janr of janri) {
-                //a
-                let li = document.createElement('li')
-                let link = document.createElement('a')
-
-                //b
-                link.classList.add('promo__menu-item')
-                link.innerHTML = janr
-                // link[0].classList.add('promo__menu-item_active')
-
-                //c
-                li.append(link)
-                ul_menu.append(li)
-
-                li.onclick = () => {
-                    link.classList.add('promo__menu-item_active')
-
-                    if (janr !== item.Genre) {
-                        promo__interactive_li = item.Title
-                        promo__interactive_li.innerHTML = ''
-                    } else {
-                        promo__interactive_li.innerHTML = idx + ')' + item.Title
-                    }
-                }
-            // }
+            setMovie(item)
         }
     }
 }
+
+// export function reload_genres(arr, place, sc_place) {
+//     place.innerHTML = ''
+
+//     for (let item of arr) {
+//         let idx = arr.indexOf(item)
+
+//         //a
+//         let li = document.createElement('li')
+//         let a = document.createElement('a')
+//         //b
+//         if (idx === 0) a.classList.add('promo__menu-item_active')
+
+//         a.classList.add('promo__menu-item')
+//         a.href = '#'
+//         a.innerHTML = item
+//         //c
+
+//         place.append(li)
+//         li.append(a)
+
+//         li.onclick = () => {
+//             const previousElem = place.querySelector('.promo__menu-item_active')
+//             previousElem.classList.remove('promo__menu-item_active')
+
+//             a.classList.add('promo__menu-item_active')
+
+//             if (item === 'Все') {
+//                 numbers(movies, sc_place)
+//                 return
+//             }
+
+//             let filtered = movies.filter(movie => movie.Genre === item)
+
+//             numbers(filtered, sc_place)
+//         }
+//     }
+// }
